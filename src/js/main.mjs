48603d739b73b1723ca76db12a14c975ce80b6c5
Char Ryder <char.ryder@blkt.co.uk>
2021-09-10 16:22:39 +0100

    Fixed slide toggle. Error with color feedback.

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -30,25 +30,23 @@ const diffEnd = document.querySelector("#diffEnd").addEventListener("change", fu
 });
 
 //slideshow
-const diffSlide = document.querySelector(".differenceBetween");
-const addSlide = document.querySelector(".addSubtract");
-
 const diffToggle = document.querySelector("#diff").addEventListener("click", function() {displayDiff()});
 const addToggle = document.querySelector("#add").addEventListener("click", function() {displayAdd()});
 
+const diffSlide = document.querySelector(".differenceBetween");
+const addSlide = document.querySelector(".addSubtract");
+
 function displayDiff(){
     diffSlide.classList.add("display");
-    diffToggle.style.backgroundColor = "#0000002f";
-    
     addSlide.classList.remove("display");
-    addToggle.style.backgroundColor = "transparent";
+
+    diffToggle.style.background = "linear-gradient(to bottom, #ccc, #fff)";
 
 }
 
 function displayAdd(){
     diffSlide.classList.remove("display");
-    diffToggle.style.backgroundColor = "transparent";
 
     addSlide.classList.add("display");
-    addToggle.style.backgroundColor = "#0000002f";
+    addToggle.style.background = "linear-gradient(to bottom, #ccc, #fff)";
 }
